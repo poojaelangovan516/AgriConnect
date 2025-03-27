d5efbfd5f2d46690df2b7cfcaad3c8e0b3eb6616
Pooja516pooja <poojasweety516@gmail.com>
2025-03-27 21:23:04 +0530

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="login" options={{ title: "login" }} />
+      <Stack.Screen name="signup" options={{ title: "signup" }} />
+      <Stack.Screen name="addProduct" options={{ title: "addProduct" }} />
+      <Stack.Screen name="completedOrders" options={{ title: "completedOrders" }} />
+      <Stack.Screen name="editProfile" options={{ title: "editProfile" }} />
+      <Stack.Screen name="inventory" options={{ title: "inventory" }} />
+      <Stack.Screen name="language" options={{ title: "language" }} />
+      <Stack.Screen name="menu" options={{ title: "menu" }} />
+      <Stack.Screen name="orderDetails" options={{ title: "orderDetails" }} />
+      <Stack.Screen name="pendingOrders" options={{ title: "pendingOrders" }} />
+      <Stack.Screen name="productList" options={{ title: "productList" }} />
+      <Stack.Screen name="settings" options={{ title: "settings" }} />
+      <Stack.Screen name="totalOrders" options={{ title: "totalOrders" }} />
+      <Stack.Screen name="updateProducts" options={{ title: "updateProducts" }} />
+    </Stack>
+  );
 }
